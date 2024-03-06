36e1065943379a1982300b8ec619cba9f051ffbc
omar <o.wakeel@intouch.com>
2024-03-07 00:44:23 +0200

    feat: make usage of search input and top filter buttons

--- src/components/StableCoinsList.tsx
+++ src/components/StableCoinsList.tsx
@@ -1,14 +1,50 @@
 "use client"
 import { IStableCoin } from '@/utilities/interfaces'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import InfiniteMovingCards from './InfiniteMovingCards'
 import Table from './Table'
+import { PegMechanismEnum } from '@/utilities/enums'
 
 interface Props {
     data: Array<IStableCoin>
 }
 
 const StableCoinsList: React.FC<Props> = ({ data }) => {
+    const [coinsInView, setCoinsInView] = useState<Array<IStableCoin>>([]);
+    const [searchText, setSearchText] = useState<string>("");
+    const [searchPegMechanism, setSearchPegMechanism] = useState<PegMechanismEnum>(PegMechanismEnum.ALL)
+
+    const getFilteredCoinsList = (pegMechanism: PegMechanismEnum, q?: string) => {
+        let filteredList = [...data]
+
+        if (pegMechanism !== PegMechanismEnum.ALL) {
+            console.log({ filteredList });
+            filteredList = filteredList.filter((row) => row.pegMechanism === pegMechanism)
+        }
+
+        if (!q) {
+            return filteredList;
+        }
+
+        return filteredList.filter((row) => {
+            return [row.name.toLocaleLowerCase(), row.symbol.toLocaleLowerCase()].findIndex((key) => key.includes(q.toLocaleLowerCase())) > -1
+        })
+    }
+
+    useEffect(() => {
+        setCoinsInView(getFilteredCoinsList(searchPegMechanism))
+    }, [])
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setCoinsInView(getFilteredCoinsList(searchPegMechanism, searchText))
+        }, 500);
+
+        return () => {
+            clearTimeout(timerId)
+        }
+    }, [searchPegMechanism, searchText])
+
     return (
         <React.Fragment>
             <InfiniteMovingCards
@@ -20,27 +56,28 @@ const StableCoinsList: React.FC<Props> = ({ data }) => {
                             key={item.id}
                         >
                             <span>
-                                {item.symbol}   {item.price > 0 && <b>{item.price} {item.pegType.replace('pegged', '')}</b>}
+                                {item.symbol}   {item.price > 0 && <b>{item.price} {item.pegMechanism.replace('pegged', '')}</b>}
                             </span>
                         </p>
                     )
                 }}
             />
 
-            <div className='my-6'>
+            <div className='my-6 w-1/2'>
                 <div className="w-full flex justify-between">
                     <div className="inline-flex overflow-hidden bg-white border divide-x rounded-lg dark:bg-gray-900 rtl:flex-row-reverse dark:border-gray-700 dark:divide-gray-700">
-                        <button className="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 bg-gray-100 sm:text-sm dark:bg-gray-800 dark:text-gray-300">
-                            View all
-                        </button>
-
-                        <button className="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 sm:text-sm dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100">
-                            Monitored
-                        </button>
-
-                        <button className="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 sm:text-sm dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100">
-                            Unmonitored
-                        </button>
+                        {
+                            [
+                                { type: PegMechanismEnum.ALL, title: 'View All' },
+                                { type: PegMechanismEnum.FIAT_BACKED, title: 'Fiat backed' },
+                                { type: PegMechanismEnum.CRYPTO_BACKED, title: 'Crypto backend' },
+                                { type: PegMechanismEnum.ALGORITHMIC, title: 'Algorithmic' },
+                            ].map((peg) => (
+                                <button onClick={() => setSearchPegMechanism(peg.type)} key={peg.type} className={`px-5 py-2 text-xs font-medium ${peg.type === searchPegMechanism ? 'text-gray-900' : 'text-gray-400'} transition-colors duration-200 bg-gray-100 sm:text-sm`}>
+                                    {peg.title}
+                                </button>
+                            ))
+                        }
                     </div>
 
                     <div className="relative flex items-center mt-4 md:mt-0">
@@ -50,19 +87,27 @@ const StableCoinsList: React.FC<Props> = ({ data }) => {
                             </svg>
                         </span>
 
-                        <input type="text" placeholder="Search" className="block w-full py-1.5 pr-5 text-gray-700 bg-white border border-gray-200 rounded-lg md:w-80 placeholder-gray-400/70 pl-11 rtl:pr-11 rtl:pl-5 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
+                        <input onChange={(e) => setSearchText(e.target.value)} value={searchText} type="text" placeholder="Search by Name, Symbol" className="block w-full py-1.5 pr-5 text-gray-700 bg-white border border-gray-200 rounded-lg md:w-80 placeholder-gray-600 pl-11 rtl:pr-11 rtl:pl-5 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
                     </div>
                 </div>
 
                 <Table columns={[
                     { key: 'name', title: 'Name' },
                     { key: 'symbol', title: 'Symbol' },
-                    { key: 'pegType', title: 'Peg Type' },
+                    {
+                        key: 'pegType',
+                        title: 'Peg Type',
+                        render: (_, row) => {
+                            return (
+                                <span>{row.pegType.replace('pegged', '')}</span>
+                            )
+                        }
+                    },
                     { key: 'pegMechanism', title: 'Peg Mechanism' },
                     {
                         key: 'chains',
                         title: 'Appears In ',
-                        render: (col, row) => {
+                        render: (_, row) => {
                             return (
                                 <span>{row.chains.length} chains</span>
                             )
@@ -71,19 +116,15 @@ const StableCoinsList: React.FC<Props> = ({ data }) => {
                     {
                         key: 'price',
                         title: 'Price',
-                        render: (col, row) => {
+                        render: (_, row) => {
                             return (
                                 <span>{row.price} {row.pegType.replace('pegged', '')}</span>
                             )
                         }
                     },
-                ]} data={data} />
+                ]} data={coinsInView} />
 
                 <div className="mt-6 sm:flex sm:items-center sm:justify-between ">
-                    <div className="text-sm text-gray-500 dark:text-gray-400">
-                        Page <span className="font-medium text-gray-700 dark:text-gray-100">1 of 10</span>
-                    </div>
-
                     <div className="flex items-center mt-4 gap-x-4 sm:mt-0">
                         <a href="#" className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md sm:w-auto gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5 rtl:-scale-x-100">
---
+++ src/utilities/enums/index.ts
@@ -0,0 +1,6 @@
+export enum PegMechanismEnum {
+    ALL = 'all',
+    FIAT_BACKED = 'fiat-backed',
+    CRYPTO_BACKED = 'crypto-backed',
+    ALGORITHMIC = 'algorithmic',
+}
