1608fb7112d1c23ef42659978ee4a9c32ff36e64
omar <o.wakeel@intouch.com>
2024-03-06 15:46:22 +0200

    feat: add stable coins responses interfaces

---
+++ src/utilities/interfaces/index.ts
@@ -0,0 +1,41 @@
+export interface IStableCoins {
+    id: string;
+    name: string;
+    symbol: string;
+    gecko_id: string;
+    pegType: string;
+    priceSource: string;
+    pegMechanism: string;
+    circulating: {
+        peggedUSD: number;
+    };
+    circulatingPrevDay: {
+        peggedUSD: number;
+    };
+    circulatingPrevWeek: {
+        peggedUSD: number;
+    };
+    circulatingPrevMonth: {
+        peggedUSD: number;
+    };
+    chainCirculating: {
+        [chain: string]: {
+            current: {
+                peggedUSD: number;
+            };
+            circulatingPrevDay: {
+                peggedUSD: number;
+            };
+            circulatingPrevWeek: {
+                peggedUSD: number;
+            };
+            circulatingPrevMonth: {
+                peggedUSD: number;
+            };
+        };
+    };
+}
+
+export interface IListStableCoinsResponse {
+    peggedAssets: IStableCoins[];
+}
