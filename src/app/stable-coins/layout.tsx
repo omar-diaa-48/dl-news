42463c2b019059130fec56d9e7d7635006216f8c
omar <o.wakeel@intouch.com>
2024-03-06 15:36:01 +0200

    feat: add stable coins page

---
+++ src/app/stable-coins/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Stable Coins List",
+  description: "DL News - List View to show stable coins",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
