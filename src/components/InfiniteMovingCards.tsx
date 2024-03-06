6627a57652e013c78f41734e49ad1fd354d01264
omar <o.wakeel@intouch.com>
2024-03-06 16:33:03 +0200

    feat: add infinite moving cards component

---
+++ src/components/InfiniteMovingCards.tsx
@@ -0,0 +1,83 @@
+import { cn } from '@/utilities/helpers/cs';
+import { IStableCoin } from '@/utilities/interfaces';
+import React, { ReactNode, useEffect, useState } from 'react'
+
+interface Props {
+    items: Array<IStableCoin>;
+    direction?: "left" | "right";
+    pauseOnHover?: boolean;
+    renderItem: (item: any) => ReactNode
+}
+
+const InfiniteMovingCards: React.FC<Props> = ({
+    items,
+    direction = "left",
+    pauseOnHover = true,
+    renderItem
+}) => {
+    const containerRef = React.useRef<HTMLDivElement>(null);
+    const scrollRef = React.useRef<HTMLUListElement>(null);
+
+    useEffect(() => {
+        addAnimation();
+    }, []);
+
+    const [start, setStart] = useState(false);
+
+    const addAnimation = () => {
+        if (containerRef.current && scrollRef.current) {
+            const scrollContent = Array.from(scrollRef.current.children);
+
+            scrollContent.forEach((item) => {
+                const duplicatedItem = item.cloneNode(true);
+                if (scrollRef.current) {
+                    scrollRef.current.appendChild(duplicatedItem);
+                }
+            });
+
+            getDirection();
+            getSpeed();
+            setStart(true);
+        }
+    }
+
+    const getDirection = () => {
+        if (containerRef.current) {
+            const animationDirection = direction === "left" ? "forwards" : "reverse"
+            containerRef.current.style.setProperty(
+                "--animation-direction",
+                animationDirection
+            );
+        }
+    };
+
+    const getSpeed = () => {
+        if (containerRef.current) {
+            const timeForCardInView = 5
+            const durationTime = items.length * timeForCardInView
+            containerRef.current.style.setProperty("--animation-duration", `${durationTime}s`);
+        }
+    };
+
+    return (
+        <div
+            ref={containerRef}
+            className="scroll w-full bg-[#E7E7E7]"
+        >
+            <ul
+                ref={scrollRef}
+                className={cn(
+                    "flex min-w-full gap-12 py-4 w-max",
+                    start && "animate-scroll ",
+                    pauseOnHover && "hover:[animation-play-state:paused]"
+                )}
+            >
+                {items.map((item) => (
+                    renderItem(item)
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default InfiniteMovingCards
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,20 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      animation: {
+        scroll:
+          "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+      },
+      keyframes: {
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          },
+        },
       },
     },
   },
   plugins: [],
 };
+
 export default config;
