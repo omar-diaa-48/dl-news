d518d0dea39141b7f0ecc9f4fb3fa28b0a8e6149
omar <o.wakeel@intouch.com>
2024-03-07 10:12:38 +0200

    feat: add required icons

---
+++ src/components/icons/ArrowUpRight.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ArrowUpRight = () => {
+    return (
+        <svg width="14px" height="14px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M7 17L17 7M17 7H8M17 7V16" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+    )
+}
+
+export default ArrowUpRight
---
+++ src/components/icons/ArrowUpShortWide.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ArrowUpShortWide = () => {
+    return (
+        <svg width="14px" height="14px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M7 3V21M7 3L11 7M7 3L3 7M14 3H15M14 9H17M14 15H19M14 21H21" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+    )
+}
+
+export default ArrowUpShortWide
